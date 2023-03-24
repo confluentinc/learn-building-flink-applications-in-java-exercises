036121b293a02575798aa59c2c50bab674ad4688
Wade Waldron <wwaldron@confluent.io>
2023-03-24 07:56:51 -0600

    Switch to curl so it will work on mac.

--- install_flink.sh
+++ install_flink.sh
@@ -1,2 +1,2 @@
-wget https://dlcdn.apache.org/flink/flink-1.17.0/flink-1.17.0-bin-scala_2.12.tgz
+curl https://dlcdn.apache.org/flink/flink-1.17.0/flink-1.17.0-bin-scala_2.12.tgz --output flink-1.17.0.tgz
 tar -xzf flink-*.tgz
