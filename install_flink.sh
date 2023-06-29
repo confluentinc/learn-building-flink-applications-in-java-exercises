006335644838c2ad2ae34040de7976681e929e86
Wade Waldron <wwaldron@confluent.io>
2023-06-29 08:02:15 -0600

    Upgrading the Flink version to 1.17.1.

--- install_flink.sh
+++ install_flink.sh
@@ -1,2 +1,2 @@
-curl https://dlcdn.apache.org/flink/flink-1.17.0/flink-1.17.0-bin-scala_2.12.tgz --output flink-1.17.0.tgz
+curl https://archive.apache.org/dist/flink/flink-1.17.1/flink-1.17.1-bin-scala_2.12.tgz --output flink-1.17.1.tgz
 tar -xzf flink-*.tgz
